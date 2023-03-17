ef3c6552f59d5662564cab9a5a42f170c31734ee
pitpat <biruktse88@gmail.com>
2023-03-17 12:59:03 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,14 @@
 /**
  * main - entry point
  *
- * Return : always 0
+ * Return: always 0
  */
 int main(void)
 {
-	printf("sizeof a char: %d byte\n", sizeof(char));
+	printf("sizeof a char: %d byte(s)\n", sizeof(char));
+	printf("sizeof an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)", sizeof(long int));
+	printf("size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("size of a float : %d byte(s)", sizeof(float));
 	return (0);
 }
