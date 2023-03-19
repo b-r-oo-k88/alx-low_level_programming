a6ce5eba0c96fd77e3f7b8bb77f08f22dca8af0d
pitpat <biruktse88@gmail.com>
2023-03-19 03:44:49 -0700

    if statement but not

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,14 @@
-#include<stdio.h>
-/**
- * main - a prgram that displays the number sign
- *
- * Return: 0 (success)
- */
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
-	scanf("%d", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n >= 0)
-		printf("%d is posetive\n", n);
-	if (n <= 0)
-		printf("%d is negative\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
