84711dfe6283eeecde38df98eeb7cc0bce4c93a2
pitpat <biruktse88@gmail.com>
2023-03-19 09:02:54 -0700

    this is if else code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - a prgram that displays the number sign
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n > 0)
+		printf("%d is posetive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d id negative\n", n);
 	return (0);
 }
