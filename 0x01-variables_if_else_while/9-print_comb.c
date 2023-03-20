a746cebae54a5be310ce5a27ade567bfb0767d99
pitpat <biruktse88@gmail.com>
2023-03-20 01:50:36 -0700

    combination of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - single digit combination
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
