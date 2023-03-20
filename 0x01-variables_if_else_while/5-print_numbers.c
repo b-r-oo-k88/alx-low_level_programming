417a52b6d7bbe677c4e37baa2244358975182eef
pitpat <biruktse88@gmail.com>
2023-03-20 01:11:14 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing 0-9
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
