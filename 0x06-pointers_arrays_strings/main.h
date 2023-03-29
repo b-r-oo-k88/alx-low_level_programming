0d0bef181156904fd9ef1cf11af5a6fa562069ac
pitpat <biruktse88@gmail.com>
2023-03-29 13:00:13 -0700

    c pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef main_h
+#define main_h
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
